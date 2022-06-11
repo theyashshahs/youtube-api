6c0d15895b4edf65d3a1bd99b5b2449d5f32e299
theyashshahs <201851150@iiitvadodara.ac.in>
2022-06-12 00:48:43 +0530

    updates pagination logic

--- youtube/tasks.py
+++ youtube/tasks.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime, timedelta
+from os import path
 
 from celery.decorators import periodic_task
 from django.conf import settings
@@ -8,8 +9,6 @@ from service_interfaces.youtube import get_youtube_searches
 from youtube.models import Youtube
 
 logger = logging.getLogger(__name__)
-
-PAGE_TOKEN = []
 
 # Async task to get youtube searches and strore them in the database
 @periodic_task(
@@ -20,11 +19,13 @@ PAGE_TOKEN = []
 )
 def youtube_search():
     # Call the youtube search API with params using youtube interface
-    published_after = datetime.now() - timedelta(days=1)
-    search_param = "cristiano ronaldo real madrid juventus"
+    published_after = datetime.now() - timedelta(days=30)
+    search_param: str = "cristiano ronaldo real madrid juventus manchester united"
+
+    youtube_object_list: list = []
 
     # Search parameters
-    params = {
+    params: dict = {
         "part": "snippet",
         "q": search_param,
         "key": settings.YOUTUBE_DATA_API_KEY,
@@ -34,36 +35,43 @@ def youtube_search():
         "maxResults": 50,
     }
 
-    if PAGE_TOKEN:
-        params["pageToken"] = PAGE_TOKEN[0]
-        PAGE_TOKEN.pop(0)
+    # Get the token from the file if exists
+    if path.exists(path.join(settings.BASE_DIR, "next_page_token.txt")):
+        with open(path.join(settings.BASE_DIR, "next_page_token.txt"), "r") as f:
+            page_token = f.read()
+            params["pageToken"] = page_token
 
-    # Get the first page results
+    # Get page results
     data = get_youtube_searches(params=params)
     if data["status"] == "success":
         logger.info("Successfully retrieved youtube search data")
-
         # Check for a next page token
         # If there is a next page token, then get the next page token
-        # store it into array
         if "nextPageToken" in data["data"]:
             next_page_token = data["data"]["nextPageToken"]
-            PAGE_TOKEN.append(next_page_token)
+
+            # Save the token in a file
+            with open(path.join(settings.BASE_DIR, "next_page_token.txt"), "w") as f:
+                f.write(next_page_token)
+                f.close()
 
         results = data["data"]["items"]
-
         # Iterate through the list and create youtube model objects
         for result in results:
             if result["id"]["videoId"] not in Youtube.objects.values_list("video_id", flat=True):
-                Youtube.objects.create(
+                obj = Youtube(
                     video_id=result["id"]["videoId"],
                     published_at=result["snippet"]["publishedAt"],
                     title=result["snippet"]["title"],
                     description=result["snippet"]["description"],
                     thumbnails=result["snippet"]["thumbnails"],
                 )
-                logger.info("Created Youtube model object")
+                youtube_object_list.append(obj)
 
     # log the errors
     else:
-        logger.error("Error retrieving youtube search data")
+        logger.error("Error retrieving youtube search data", data["data"])
+
+    # Save the youtube model objects
+    Youtube.objects.bulk_create(youtube_object_list)
+    logger.info("Created %s youtube model objects" % len(youtube_object_list))
